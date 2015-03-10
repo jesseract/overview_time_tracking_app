bb049fd09282db817a6bb0b5589288d3dc172816
Jesseract <jlpearce@gmail.com>
2015-03-10 17:05:08 -0400

    Add hours

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -1,23 +1,50 @@
 class HoursController < ApplicationController
-  def new
-  end
+  before_action :set_hours, only: [:show, :edit, :update, :destroy]
 
-  def index
-  end
 
-  def edit
-  end
+    def new
+      @hour = Hour.new
+    end
 
-  def create
-  end
+    def index
+      @hours = Hour.all
+    end
 
-  def show
-  end
+    def edit
+    end
 
-  def update
-  end
+    def create
+      @hour = Hour.new(hour_params)
+      if @hour.save
+        redirect_to hours_path, notice: "Hour was successfully created"
+      else
+        render 'new', notice: "Try again"
+      end
+    end
 
-  def destroy
-  end
+    def show
+    end
 
-end
+    def update
+      if @hour.update(hour_params)
+        redirect_to hours_path, notice: 'Hour was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @hour.destroy
+      redirect_to hours_url, notice: 'Hour was successfully destroyed.'
+    end
+
+    private def set_developer
+        @hour = Hour.find(params[:id])
+      end
+
+      def hour_params
+        params.require(:hour).permit(:worked_on, :duration)
+      end
+
+
+  end
